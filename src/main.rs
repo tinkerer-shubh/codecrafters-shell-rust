3a9decd1f56a723170807e1997efca3a651eaaf6
shubham humbe <concept.tinker@gmail.com>
2026-01-05 23:28:27 +0530

    running external programs

--- src/main.rs
+++ src/main.rs
@@ -2,95 +2,113 @@
 use std::io::{self, Write};
 use std::{env, fs};
 use std::path::PathBuf;
+use std::process::Command;
 
 #[cfg(unix)]
 use std::os::unix::fs::PermissionsExt;
+
+fn find_executable_in_path(name: &str) -> Option<PathBuf> {
+    let path_os = env::var_os("PATH")?;
+    for dir in env::split_paths(&path_os) {
+        let candidate = dir.join(name);
+
+        let metadata = match fs::metadata(&candidate) {
+            Ok(m) => m,
+            Err(_) => continue,
+        };
+
+        if !metadata.is_file() {
+            continue;
+        }
+
+        #[cfg(unix)]
+        {
+            if metadata.permissions().mode() & 0o111 == 0 {
+                continue; // exists but not executable
+            }
+        }
+
+        return Some(candidate);
+    }
+    None
+}
 
 fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut command = String::new();
-        if io::stdin().read_line(&mut command).unwrap() == 0 {
+        let mut command_line = String::new();
+        if io::stdin().read_line(&mut command_line).unwrap() == 0 {
             break;
         }
 
-        let cmd = command.trim();
+        let cmd = command_line.trim();
         if cmd.is_empty() {
             continue;
         }
 
         let parts: Vec<&str> = cmd.split_whitespace().collect();
+        let program = parts[0];
 
-        /* ================= exit ================= */
-        if parts[0] == "exit" {
-            break;
+        // exit
+        if program == "exit"
+        break;
+    }
+    
+    // echo
+    if program == "echo" {
+        if parts.len() > 1{
+            println!("{}", parts[1..].join(" "));
+        } else {
+            println!();
         }
+        continue;
+    }
 
-        /* ================= echo ================= */
-        if parts[0] == "echo" {
-            if parts.len() > 1 {
-                println!("{}", parts[1..].join(" "));
-            } else {
-                println!();
-            }
+    // type
+    if program == "type" {
+        if parts.len() < 2 {
+            println!("type: not found");
             continue;
         }
 
-        /* ================= type ================= */
-        if parts[0] == "type" {
-            if parts.len() < 2 {
-                println!("type: not found");
-                continue;
-            }
+        let target = parts[1];
 
-            let target = parts[1];
-
-            // builtins
-            if matches!(target, "echo" | "exit" | "type") {
-                println!("{} is a shell builtin", target);
-                continue;
-            }
-
-            // search PATH
-            let mut found: Option<PathBuf> = None;
-
-            if let Some(path_os) = env::var_os("PATH") {
-                for dir in env::split_paths(&path_os) {
-                    let candidate = dir.join(target);
-
-                    let metadata = match fs::metadata(&candidate) {
-                        Ok(m) => m,
-                        Err(_) => continue,
-                    };
-
-                    if !metadata.is_file() {
-                        continue;
-                    }
-
-                    #[cfg(unix)]
-                    {
-                        if metadata.permissions().mode() & 0o111 == 0 {
-                            continue;
-                        }
-                    }
-
-                    found = Some(candidate);
-                    break;
-                }
-            }
-
-            if let Some(path) = found {
-                println!("{} is {}", target, path.display());
-            } else {
-                println!("{}: not found", target);
-            }
-
+        // buitins
+        if matches!(target, "echo" | "exit" | "type") {
+            println!("{} is a shell builtin", target);
             continue;
         }
 
-        /* ================= fallback ================= */
-        println!("{}: command not found", cmd);
+        // path execs
+        if let Some(path) = find_executable_in_path(target) {
+            println!("{ is {}", target, path.display());
+        } else {
+            println!("{}: not found", target);
+        }
+
+        continue;
+    }
+
+    // external programs
+    if let Some(path) = find_executable_in_path(program) {
+        let mut child_cmd = Command::new(&path);
+
+        child_cmd.arg0(program);
+
+        if parts.len() > 1 {
+            child_cmd.args(&parts[1..]);
+        }
+
+        match child_cmd.status() {
+            Ok(_status) => {}
+            Err(_e) => {
+                // if exec fails for some reason
+                println!("{}: command not found", program);
+            }
+        } else {
+            println!("{: command not found", cmd);
+        }
     }
 }
