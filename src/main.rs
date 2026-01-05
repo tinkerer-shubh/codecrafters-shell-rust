03ebfcc53e0555a619d178288d86f150077b9daa
shubham humbe <concept.tinker@gmail.com>
2026-01-05 21:35:06 +0530

    REPL

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,18 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
+     // TODO: Uncomment the code below to pass the first stage
+    loop{
     print!("$ ");
      io::stdout().flush().unwrap();
 
      let mut command = String::new();
      io::stdin().read_line(&mut command).unwrap();
-     println!("{}: command not found", command.trim());
+
+     if command.is_empty(){
+        continue;
+     }
+          println!("{}: command not found", command.trim());
 }
+}
+   
